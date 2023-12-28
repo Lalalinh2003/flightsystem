311979b4708761bc3bfba6c89b729d44a25404a8
bnson1986 <148788387+bnson1986@users.noreply.github.com>
2023-12-28 18:35:36 +0700

    Update index.py

--- app/index.py
+++ app/index.py
@@ -12,9 +12,9 @@ def login():
     return render_template("login.html")
 
 #Duong dan den trang dat ve
-@flightapp.route("/booktickets")
-def booktickets():
-    return render_template("booktickets.html")
+@flightapp.route("/book_tickets")
+def book_tickets():
+    return render_template("book_tickets.html")
 
 if __name__ == '__main__':
     flightapp.run(debug=True)
