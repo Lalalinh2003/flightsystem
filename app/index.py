0aba3482806d95ba40b565dc20b3c50f3791ec67
bnson1986 <developer.bnson@gmail.com>
2024-01-05 00:20:53 +0700

    modified:   app/index.py 	modified:   app/static/css/style.css 	modified:   app/templates/book_tickets.html 	new file:   app/templates/buy_tickets.html

--- app/index.py
+++ app/index.py
@@ -1,14 +1,16 @@
+from datetime import datetime, time, timedelta
 from flask import Flask, render_template, redirect, url_for, request, flash
 from urllib.parse import quote
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, or_
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_admin import Admin, BaseView, AdminIndexView, expose
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 from flask_admin.contrib.sqla import ModelView
-from wtforms import FloatField, SelectField
+from wtforms import DateField, FloatField, SelectField, StringField
 from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
+from sqlalchemy import and_
 
 flightapp = Flask(__name__)
 
@@ -78,10 +80,81 @@ def login():
             return render_template("login.html", current_user=current_user)
 
 # Duong dan den trang dat ve
-@flightapp.route("/book_tickets")
+@flightapp.route("/book_tickets", methods=['GET', 'POST'])
 #@login_required
 def book_tickets():
-    return render_template("book_tickets.html", current_user=current_user)
+    airport_origin = request.args.get('airport_origin')
+    airport_destination = request.args.get('airport_destination')
+    airport_departure_date = request.args.get('airport_departure_date')
+
+    twelve_hours_ago = datetime.now() - timedelta(hours=10)
+    print(twelve_hours_ago)
+    flights = Flight.query.filter(and_(Flight.departure_time >= twelve_hours_ago), or_(Flight.available_seats_class_1 > 0, Flight.available_seats_class_2 > 0)).all()    
+
+    formSearchFlight = FormSearchFlight()
+    if formSearchFlight.validate_on_submit():
+        origin_id = formSearchFlight.origin.data
+        destination_id = formSearchFlight.destination.data
+        departure_date = formSearchFlight.departure_date.data
+        #print(origin_id)
+        #print(destination_id)
+        #print(departure_date)
+        
+        # Do something with the search parameters
+        #flights = Flight.query.filter_by(origin_id=origin_id, destination_id=destination_id).all()
+        flights = Flight.query.join(Route, Flight.route_id == Route.id).filter(Route.origin_id==origin_id, Route.destination_id==destination_id, Flight.departure_time >= twelve_hours_ago, or_(Flight.available_seats_class_1 > 0, Flight.available_seats_class_2 > 0)).all()
+
+    return render_template("book_tickets.html", current_user=current_user, flights=flights, formSearchFlight=formSearchFlight)
+
+@flightapp.route("/buy_tickets", methods=['POST'])
+def buy_tickets():
+    if request.method == 'POST':
+        #print(request.form)
+        flightId = request.form['flight_id']
+        flightCode = request.form['flight_code']
+        routeId = request.form['route_id']
+        originName = request.form['origin_name']
+        destinationName = request.form['destination_name']
+        departureTime = request.form['departure_time']
+        arrivalTime = request.form['arrival_time']
+        seatClass = request.form['seat_class']
+        seatPrice = request.form['seat_price']
+
+        return render_template('buy_tickets.html', flightId=flightId, flightCode=flightCode, routeId=routeId, originName=originName, destinationName=destinationName, departureTime=departureTime, arrivalTime=arrivalTime, seatClass=seatClass, seatPrice=seatPrice)
+    
+    return redirect(url_for('book_tickets'))
+
+@flightapp.route("/save_ticket", methods=['POST'])
+def save_ticket():
+    flightId = request.form['flightId']
+    #flightCode = request.form['flightCode']
+    #routeId = request.form['routeId']
+    #originName = request.form['originName']
+    #destinationName = request.form['destinationName']
+    #departureTime = request.form['departureTime']
+    #arrivalTime = request.form['arrivalTime']
+    seatClass = request.form['seatClass']
+    seatPrice = request.form['seatPrice']
+    #employeeId = request.form['employeeId']
+
+    fullName = request.form['fullName']
+    identityCard = request.form['identityCard']
+    phoneNumber = request.form['phoneNumber']
+    address = request.form['address']
+    bankNumber = request.form['bankNumber']
+
+    #-- save customer
+    customer = Customer(full_name=fullName, identity_card=identityCard, address=address, phone_number=phoneNumber, bank_number=bankNumber)
+    db.session.add(customer)
+    db.session.commit()
+    customerId = customer.id
+
+    #-- save ticket
+    ticket = Ticket(flight_id=flightId, customer_id=customerId, employee_id=None, seat_class=seatClass, seat_price=seatPrice)
+    db.session.add(ticket)
+    db.session.commit()
+
+    return redirect('/')
 
 #==========================
 #==========================
@@ -110,6 +183,16 @@ class Employee(db.Model):
     full_name = db.Column(db.String(100), nullable=False)
     position = db.Column(db.String(50), nullable=False)
     #user = db.relationship('User', foreign_keys=[user_id])
+
+class Customer(db.Model):
+    __tablename__ = 'customer'
+    __table_args__ = {'mysql_engine':'InnoDB'}
+    id = Column(db.Integer, primary_key=True, autoincrement=True)
+    full_name = Column(db.String(100), nullable=False)
+    identity_card = Column(db.String(20), nullable=False)
+    address = Column(db.String(200), nullable=False)
+    phone_number = Column(db.String(20), nullable=False)
+    bank_number = Column(db.String(20), nullable=False)
 
 class Airport(db.Model):
     __tablename__ = 'airport'
@@ -192,6 +275,47 @@ class Flight(db.Model):
     route = db.relationship('Route', foreign_keys=[route_id])
     #transit_flights = db.relationship('TransitFlights', backref='flight', lazy=True)
 
+def get_routes():
+    return Route.query.all()
+
+def get_route_pk(route):
+    return route.id
+
+def get_route_label(route):
+    return f'{route.origin.name} - {route.destination.name}'
+
+class FlightModelView(AuthenticatedView):
+    can_create = True
+    can_edit = True
+    can_delete = True
+    column_list = ['id', 'code', 'route.origin.name', 'route.destination.name', 
+                   'departure_time', 'arrival_time',
+                   'num_seats_class_1', 'num_seats_class_2',
+                   'available_seats_class_1', 'available_seats_class_2',
+                   'price_seat_class_1', 'price_seat_class_2']    
+    form_columns = ['code', 'route', 
+                   'departure_time', 'arrival_time',
+                   'num_seats_class_1', 'num_seats_class_2',
+                   'available_seats_class_1', 'available_seats_class_2',
+                   'price_seat_class_1', 'price_seat_class_2']
+    column_searchable_list = ['code', 'route_id', 'departure_time', 'arrival_time']
+    column_filters = ['code', 'route_id', 'departure_time', 'arrival_time']
+    page_size = 50       
+
+    column_labels = {
+        'route.origin.name': 'Sân bay xuất phát',
+        'route.destination.name': 'Sân bay đích',
+    }   
+
+    form_args = {
+        'route': {
+            'label': 'Route',
+            'query_factory': get_routes,
+            'get_pk': get_route_pk,
+            'get_label': get_route_label
+        }
+    }
+
 class TransitFlight(db.Model):
     __tablename__ = 'transit_flight'
     __table_args__ = {'mysql_engine':'InnoDB'}
@@ -230,6 +354,22 @@ class AdminView(AdminIndexView):
     def inaccessible_callback(self, name, **kwargs):
         # redirect to login page if user doesn't have access
         return redirect(url_for('login', next=request.url))    
+
+class Ticket(db.Model):
+    __tablename__ = 'ticket'
+    __table_args__ = {'mysql_engine':'InnoDB'}
+    id = db.Column(db.Integer, primary_key=True)
+    flight_id = db.Column(db.Integer, db.ForeignKey('flight.id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=True)
+    seat_class = db.Column(db.String(50), nullable=False)
+    seat_price = db.Column(db.Float, nullable=False)
+
+    flight = db.relationship('Flight', foreign_keys=[flight_id])
+    customer = db.relationship('Customer', foreign_keys=[customer_id])
+    employee = db.relationship('Employee', foreign_keys=[employee_id])
+
+
 #==========================
 #==========================
 with flightapp.app_context():
@@ -287,12 +427,31 @@ with flightapp.app_context():
     db.session.add_all(routes)
     db.session.commit()
 
+    flights = [
+        Flight(code='FLY001', route_id='1', departure_time='2023-12-03 01:00:00', arrival_time='2023-12-03 03:30:00', num_seats_class_1='10', num_seats_class_2='100', available_seats_class_1='10', available_seats_class_2='100', price_seat_class_1='1000000', price_seat_class_2='100000'),
+        Flight(code='FLY002', route_id='2', departure_time='2024-01-04 01:00:00', arrival_time='2024-01-04 03:30:00', num_seats_class_1='20', num_seats_class_2='200', available_seats_class_1='20', available_seats_class_2='200', price_seat_class_1='2000000', price_seat_class_2='200000'),
+        Flight(code='FLY003', route_id='3', departure_time='2024-01-05 01:00:00', arrival_time='2024-01-05 03:30:00', num_seats_class_1='30', num_seats_class_2='300', available_seats_class_1='30', available_seats_class_2='300', price_seat_class_1='3000000', price_seat_class_2='300000'),
+        Flight(code='FLY004', route_id='4', departure_time='2024-01-06 01:00:00', arrival_time='2024-01-06 03:30:00', num_seats_class_1='40', num_seats_class_2='400', available_seats_class_1='40', available_seats_class_2='400', price_seat_class_1='4000000', price_seat_class_2='400000'),
+    ]
+    db.session.add_all(flights)
+    db.session.commit()    
+
 #==========================
 #==========================
 admin = Admin(flightapp, name='Quản Lý', template_mode='bootstrap3', index_view=AdminView(name='Trang Chủ'))
 admin.add_view(AirportModelView(Airport, db.session, name='Sân Bay'))
 admin.add_view(RouteModelView(Route, db.session, name='Tuyến Bay'))
+admin.add_view(FlightModelView(Flight, db.session, name='Chuyến Bay'))
 admin.add_view(LogoutView(name="Đăng xuất"))
+
+#==========================
+#==========================
+class FormSearchFlight(FlaskForm):
+    with flightapp.app_context():
+        origin = SelectField('Origin', choices=[(airport.id, airport.name) for airport in Airport.query.all()], validators=[DataRequired()])
+        destination = SelectField('Destination', choices=[(airport.id, airport.name) for airport in Airport.query.all()], validators=[DataRequired()])
+        departure_date = DateField('Departure date', format='%Y-%m-%d', validators=[DataRequired()])
+
 #==========================
 #==========================
 if __name__ == '__main__':
